9042f6178c5a7b69cf743536f5f2ed36f3c32e44
Daria <nowickadaria1@gmail.com>
2017-10-07 16:20:09 +0200

    Added static HTML page

---
+++ server.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Daria on 07.10.2017.
+ */
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+var PORT = 3000;
+
+app.set('port', process.env.PORT || PORT);
+
+app.set('views', path.resolve(__dirname, 'static', 'views'));
+app.set('view engine', 'ejs');
+
+app.use('/', express.static(path.join(__dirname, 'static')));
+
+app.get('/', function (req, res) {
+    res.render("index");
+});
+
+app.listen(app.get('port'), function () {
+    console.log('App started on port 3000.');
+});
