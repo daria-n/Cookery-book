904eb7975ad30209731f7709cb6e8c9061011d55
Daria <nowickadaria1@gmail.com>
2017-10-17 21:55:47 +0200

    Navigation bar is working - using AngularJS routeProvider

--- static/js/app.js
+++ static/js/app.js
@@ -1,17 +1,35 @@
 /**
  * Created by Daria on 07.10.2017.
  */
-var triangle_hidden = "&#9658;";
-var triangle_displayed = "&#9660;";
+// var triangle_hidden = "&#9658;";
+// var triangle_displayed = "&#9660;";
+//
+// $(".toggle-recipe").click(function () {
+//     var recipe_content = $(this).parent().parent().next('div')[0];
+//     if (recipe_content.classList.contains('hidden')) {
+//         recipe_content.classList.remove('hidden');
+//         $(this).html(triangle_displayed);
+//     }
+//     else {
+//         recipe_content.classList.add('hidden');
+//         $(this).html(triangle_hidden);
+//     }
+// });
 
-$(".toggle-recipe").click(function () {
-    var recipe_content = $(this).parent().parent().next('div')[0];
-    if (recipe_content.classList.contains('hidden')) {
-        recipe_content.classList.remove('hidden');
-        $(this).html(triangle_displayed);
-    }
-    else {
-        recipe_content.classList.add('hidden');
-        $(this).html(triangle_hidden);
-    }
-});
+angular.module("cookeryBookApp", ['ngRoute'])
+    .config(['$routeProvider',
+        function ($routeProvider) {
+            $routeProvider
+                .when('/main', {
+                    templateUrl: 'views/_main.ejs'
+                })
+                .when('/spaghetti', {
+                    templateUrl: 'views/_spaghetti.ejs'
+                })
+                .when('/biszkopt', {
+                    templateUrl: 'views/_biszkopt.ejs'
+                })
+                .otherwise({
+                    redirectTo: '/main'
+                });
+        }]);
