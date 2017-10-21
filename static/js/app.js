51b323f542a2f419a7098923600e54f5393faa95
Daria <nowickadaria1@gmail.com>
2017-10-21 21:03:10 +0200

    Removed obsolete commented code

--- static/js/app.js
+++ static/js/app.js
@@ -1,20 +1,6 @@
 /**
  * Created by Daria on 07.10.2017.
  */
-// var triangle_hidden = "&#9658;";
-// var triangle_displayed = "&#9660;";
-//
-// $(".toggle-recipe").click(function () {
-//     var recipe_content = $(this).parent().parent().next('div')[0];
-//     if (recipe_content.classList.contains('hidden')) {
-//         recipe_content.classList.remove('hidden');
-//         $(this).html(triangle_displayed);
-//     }
-//     else {
-//         recipe_content.classList.add('hidden');
-//         $(this).html(triangle_hidden);
-//     }
-// });
 
 angular.module("cookeryBookApp", ['ngRoute'])
     .config(['$routeProvider',
