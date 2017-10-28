701e84f84d5fde3d8a70bc6ef8e6f28d5cffe657
Daria <nowickadaria1@gmail.com>
2017-10-28 22:19:36 +0200

    Moved inline functions to separate function declarations

--- static/js/app.js
+++ static/js/app.js
@@ -3,68 +3,70 @@
  */
 
 angular.module("cookeryBookApp", ['ngRoute'])
-    .config(['$routeProvider',
-        function ($routeProvider) {
-            $routeProvider
-                .when('/main', {
-                    templateUrl: 'views/_main.ejs',
-                    controller: 'mainController',
-                    controllerAs: 'main'
-                })
-                .when('/:name', {
-                    templateUrl: 'views/_recipe_detail.ejs',
-                    controller: 'mainController',
-                    controllerAs: 'main'
-                })
-                .otherwise({
-                    redirectTo: '/main'
-                });
-        }])
+    .config(['$routeProvider', routing])
     .service("recipesService", ['$http', function ($http) {
         return $http.get('/recipes');
     }])
-    .controller("mainController", ['$scope', '$location', 'recipesService', function ($scope, $location, recipesService) {
-        var vm = this;
+    .controller("mainController", ['$location', 'recipesService', mainCtrlFcn])
+    .filter('searchByName', searchByNameFilterFcn);
 
-        vm.clearInput = function(){
-            vm.searchString = "";
-        };
-
-        vm.recipeUrl = $location.path().substr(1);
-
-        recipesService.then(function (data) {
-            vm.posts = data.data;
-
-            var getCurrentRecipe = function (UrlName) {
-                return vm.posts.filter(function (obj) {
-                    return obj.UrlName === UrlName;
-                });
-            };
-
-            if (vm.recipeUrl !== "main")
-                vm.currentRecipe = getCurrentRecipe(vm.recipeUrl)[0];
+function routing($routeProvider) {
+    $routeProvider
+        .when('/main', {
+            templateUrl: 'views/_main.ejs',
+            controller: 'mainController',
+            controllerAs: 'main'
+        })
+        .when('/:name', {
+            templateUrl: 'views/_recipe_detail.ejs',
+            controller: 'mainController',
+            controllerAs: 'main'
+        })
+        .otherwise({
+            redirectTo: '/main'
         });
+}
 
-        vm.buildUrl = function (UrlName) {
-            return "#!" + UrlName;
+function mainCtrlFcn($location, recipesService) {
+    var vm = this;
+
+    vm.clearInput = function () {
+        vm.searchString = "";
+    };
+
+    vm.recipeUrl = $location.path().substr(1);
+
+    recipesService.then(function (data) {
+        vm.posts = data.data;
+
+        var getCurrentRecipe = function (UrlName) {
+            return vm.posts.filter(function (obj) {
+                return obj.UrlName === UrlName;
+            });
         };
 
-        vm.buildPathToImg = function (UrlName) {
-            return "../img/" + UrlName + ".jpg";
-        };
-
-    }])
-    .filter('searchByName', function () {
-        return function (arr, searchString) {
-            var result = [];
-            if (searchString) {
-                searchString = searchString.toLowerCase();
-
-                angular.forEach(arr, function (item) {
-                    if (item.name.toLowerCase().indexOf(searchString) !== -1)
-                        result.push(item);
-                });
-            }
-            return result;
-        };
+        if (vm.recipeUrl !== "main")
+            vm.currentRecipe = getCurrentRecipe(vm.recipeUrl)[0];
     });
+
+    vm.buildUrl = function (UrlName) {
+        return "#!" + UrlName;
+    };
+
+    vm.buildPathToImg = function (UrlName) {
+        return "../img/" + UrlName + ".jpg";
+    };
+}
+
+function searchByNameFilterFcn() {
+    return function (arr, searchString) {
+        var result = [];
+        if (searchString) {
+            angular.forEach(arr, function (item) {
+                if (item.name.toLowerCase().indexOf(searchString.toLowerCase()) !== -1)
+                    result.push(item);
+            });
+        }
+        return result;
+    };
+}
